4438c03b77515f83271d702778f799b1affe93fe
Phil-Andre <pakueamb@gmail.com>
2021-02-13 02:24:07 -0500

    created template data for blocks

---
+++ Space Journey Breakout/Assets/Scripts/Block Scripts/BlockData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Template that contains value for types of blocks
+/// </summary>
+
+[CreateAssetMenu(fileName = "BlockFile", menuName = "BlockFile")]
+
+public class BlockData : ScriptableObject
+{
+    [Header("Block Information")]
+    public bool breakable = true;    // if block can be broken
+    public int blockPoints = 0;  // points awarded when block is hit
+
+    [Space(10)]
+    [Tooltip("Size of this array determines the number of hits block can take")]
+    public Sprite[] blockPhases; // number of phases = # of hit points
+
+    [Space(10)]
+    public Color blockColor; // color for that type of block
+}
